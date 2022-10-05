6acba221ca184901ba1f2fadfcb82be7c92183c9
usui <ctct@gmail.com>
2022-10-05 15:23:04 +0900

    忍具一覧作成完了

--- src/main/java/com/example/app/domain/ItemType.java
+++ src/main/java/com/example/app/domain/ItemType.java
@@ -1,11 +1,15 @@
 package com.example.app.domain;
 
+import java.util.Date;
+
 import lombok.Data;
 
 @Data
 public class ItemType {
 	
 	private Integer id;
-	private String name;
+	private String typesId;
+	private String work;
+	private Date postDate;
 
 }
